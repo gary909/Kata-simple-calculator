84ecce8e7322e10de4c5aeabfd83b7f7d6b8c56e
gary909 <garywhite61@hotmail.com>
2022-12-05 13:35:09 +0100

    code working

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 function calculator(a, b, sign) {
-
+    if (isNaN(a) || isNaN(b)) {
+        return "unknown value";
+    } else if (sign == "+") {
+        return a + b;
+    } else if (sign == "-") {
+        return a - b;
+    } else if (sign == "*") {
+        return a * b;
+    } else if (sign == "/") {
+        return a / b;
+    } else {
+        return "unknown value";
+    }
 }
+
+
 
 console.log(calculator(1, 2, "+")); // 3
 console.log(calculator(1, 2, "-")); // -1
