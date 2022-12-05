9602d5b6c44095ca67f7f769bc9f9294cfc06d47
gary909 <garywhite61@hotmail.com>
2022-12-05 13:13:49 +0100

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function calculator(a, b, sign) {
+
+}
+
+console.log(calculator(1, 2, "+")); // 3
+console.log(calculator(1, 2, "-")); // -1
+console.log(calculator(3, 5, "*")); // 15
+console.log(calculator(6, 2, "/")); // 3
+console.log(calculator(6, 2, "$")); // "unknown value"
+console.log(calculator(6, "h", "*")); // "unknown value"
